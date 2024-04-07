7ba8cfae5f0695e8a4849cfccdc62b9f4b6d3ec8
neilsatsangi <neil.satsangi24@gmail.com>
2024-04-07 17:13:17 -0400

    updated data files

--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -1,11 +1,47 @@
 export default [
-    { "_id": "A101", "title": "ENV + HTML", "course": "CS101" , "points": "100", "duedate": "Jan 15", "duetime": "11:59pm"},
-    { "_id": "A102", "title": "CSS + BOOTSTRAP", "course": "CS101", "points": "85", "duedate": "Feb 1" , "duetime": "11:59pm"},
-    { "_id": "A103", "title": "JS + REACT", "course": "CS101", "points": "75", "duedate": "Feb 15", "duetime": "11:59pm" },
-    { "_id": "A201", "title": "STATE + REDUX", "course": "CS102", "points": "100", "duedate": "Feb 25", "duetime": "11:59pm" },
-    { "_id": "A202", "title": "NODE + SESSION", "course": "CS101", "points": "100", "duedate": "Mar 5", "duetime": "11:59pm" },
-    { "_id": "A203", "title": "MONGO", "course": "CS102", "points": "100", "duedate": "Mar 15", "duetime": "11:59pm" },
-    { "_id": "A301", "title": "DYNAMODB", "course": "CS103", "points": "100", "duedate": "Mar 25", "duetime": "11:59pm" },
-    { "_id": "A302", "title": "TYPESCRIPT", "course": "CS103", "points": "100", "duedate": "Apr 5", "duetime": "11:59pm" },
-    { "_id": "A303", "title": "FINAL PROJECT", "course": "CS103", "points": "100", "duedate": "Apr 15", "duetime": "11:59pm" }
-  ]
+  {
+    _id: "6557fcb7a11aca237708374b",
+    title: "Propulsion Assignment",
+    course: "RS101",
+  },
+  {
+    _id: "6557fcb7a11aca237708374c",
+    title: "Combustion Analysis",
+    course: "RS101",
+  },
+  {
+    _id: "6557fcb7a11aca237708374d",
+    title: "Nozzle Design Project",
+    course: "RS101",
+  },
+  {
+    _id: "6557fcb7a11aca237708374e",
+    title: "Aerodynamics Quiz",
+    course: "RS102",
+  },
+  {
+    _id: "6557fcb7a11aca237708374f",
+    title: "Flow Analysis",
+    course: "RS102",
+  },
+  {
+    _id: "6557fcb7a11aca2377083750",
+    title: "Heating Analysis",
+    course: "RS102",
+  },
+  {
+    _id: "6557fcb7a11aca2377083751",
+    title: "Structural Design Task",
+    course: "RS103",
+  },
+  {
+    _id: "6557fcb7a11aca2377083752",
+    title: "Orbital Calculations",
+    course: "RS103",
+  },
+  {
+    _id: "6557fcb7a11aca2377083753",
+    title: "Systems Engineering Exam",
+    course: "RS103",
+  },
+];
