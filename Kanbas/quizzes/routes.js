bcccc430afe07c48201f7f0d60fb57b66ec9273b
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2024-04-09 18:12:33 -0400

    added stuff

--- Kanbas/quizzes/routes.js
+++ Kanbas/quizzes/routes.js
@@ -1,23 +1,23 @@
 import db from "../Database/index.js";
 function QuizRoutes(app) {
-    app.post("/api/courses/:cid/quizzes", (req, res) => {
-        const { cid } = req.params;
-        const newQuiz = {
-          ...req.body,
-          course: cid,
-          _id: new Date().getTime().toString(),
-        };
-        db.quizzes.push(newQuiz);
-        res.send(newQuiz);
-      });
-    
+  app.post("/api/courses/:cid/quizzes", (req, res) => {
+    const { cid } = req.params;
+    const newQuiz = {
+      ...req.body,
+      course: cid,
+      _id: new Date().getTime().toString(),
+    };
+    db.quizzes.push(newQuiz);
+    res.send(newQuiz);
+  });
+
   app.get("/api/courses/:cid/quizzes", (req, res) => {
     const { cid } = req.params;
     const quizzes = db.quizzes
       .filter((q) => q.course === cid);
     res.send(quizzes);
   });
-  app.delete("/api/quizzes/:qid", (req, res) => {
+  app.delete("/api/quizzes/delete/:qid", (req, res) => {
     const { qid } = req.params;
     db.quizzes = db.quizzes.filter((q) => q._id !== qid);
     res.sendStatus(200);
@@ -31,6 +31,13 @@ function QuizRoutes(app) {
       ...req.body
     };
     res.sendStatus(204);
+  });
+  app.put("/api/quizzes/publish/:qid", (req, res) => {
+    const { qid } = req.params;
+    const quizIndex = db.quizzes.findIndex(
+      (q) => q._id === qid);
+    db.quizzes[quizIndex].published = !db.quizzes[quizIndex].published;
+    res.send(db.quizzes[quizIndex].published);
   });
 }
 
